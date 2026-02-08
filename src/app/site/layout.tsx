06313a7ec1880bbcc127f50b5db7a6fca222fdee
sun4501 <88189687+sonjame@users.noreply.github.com>
2026-02-08 22:20:52 +0900

    Update layout.tsx

--- src/app/site/layout.tsx
+++ src/app/site/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'School Plus',
+ title: '스쿨 플러스 (School Plus)',
   description: '학생을 위한 학교 커뮤니티 플랫폼',
   verification: {
     google: 'NegZrvdc4HmzlqsjhSgXMqt9sWsRLPOWDvdGAHk2iKY', // ← content 값만
