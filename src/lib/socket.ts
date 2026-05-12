e39d7fabdd523edbf7d4c95888d2974bb995f19c
sun4501 <88189687+sonjame@users.noreply.github.com>
2026-05-13 00:13:26 +0900

    Add authentication token to socket connection

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -3,4 +3,13 @@ import { io } from 'socket.io-client'
 export const socket = io(process.env.NEXT_PUBLIC_API_URL!, {
   transports: ['websocket'],
   autoConnect: false,
+
+  withCredentials: true,
+
+  auth: {
+    token:
+      typeof window !== 'undefined'
+        ? localStorage.getItem('accessToken')
+        : null,
+  },
 })
